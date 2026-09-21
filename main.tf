6ab02a3474b790f584d33071568ab50e2471b298
Ankit-kumar28 <ankitk123907@gmail.com>
2026-09-21 10:55:19 +0530

    Terraform multi environment project

---
+++ main.tf
@@ -0,0 +1,48 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_instance" "app" {
+  count = var.instance_count
+
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  subnet_id = element(
+    data.aws_subnets.default.ids,
+    count.index
+  )
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-${count.index + 1}"
+    Environment = var.environment
+  }
+}
